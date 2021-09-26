12b95984deb45f29045ac52a866fc2810831be5d
KianYang-Lee <ken.lee.kianyang@gmail.com>
2021-09-26 21:00:02 +0800

    Correct error display

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name == 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
   } else if (error.name == 'ValidationError') {
-    return response.status(400).send({ error: 'Duplicated entry' });
+    return response.status(400).send({ error: 'Validation error' });
   }
 };
 app.use(errorHandler);
